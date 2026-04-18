038fefd153eb3196ed1b5626678b14bf69450dc7
NAV-ON-WINDOWS <arnav.salgarkar@gmail.com>
2026-04-19 00:08:45 +0530

    fixed variable naming w/ reference to pandas nomenclature

--- unfiltered/main.py
+++ unfiltered/main.py
@@ -9,7 +9,6 @@ pd.set_option('display.max_columns', None)
 # checking for null values
 n_null = df.isnull()
 # print(n_null.sum()) # there are no null values in the dataset
-
 """
 since there are no null values,
 we don't need to fill none values,
@@ -18,13 +17,15 @@ replace the duplicates,
 """
 
 # counting the occurrences of the word "education" across the dataset
-education = df.headline_text.str.contains("education", case=True,
+education_mask = df.headline_text.str.contains("education", case=True,
                                      na=None, regex=False)
-print(f"{df[education].shape[0]} headlines found!") # 9299 headlines
-print(f"{df[education].head(9299)}") # printing all the 9299 headlines found
+# print(f"{df[education_mask].shape[0]} headlines found!") # 9299 headlines
+# print(f"{df[education_mask].head(9299)}") # printing all the 9299 headlines found
+
+
 
 # counting the occurrences of the word "employment" across the dataset
-employment = df.headline_text.str.contains("employment", case=True,
+employment_mask = df.headline_text.str.contains("employment", case=True,
                                            na=None, regex=False)
-# print(f"{df[employment].shape[0]} headlines found!") # 1008 headlines found
-# print(f"{df[employment].head()}") # printing all the 1008 headlines found
+# print(f"{df[employment_mask].shape[0]} headlines found!") # 1008 headlines found
+# print(f"{df[employment_mask].head()}") # printing all the 1008 headlines found
