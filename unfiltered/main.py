921a7bc9e845dff1a7fdee41dfea057cd1ca2a55
NAV-ON-WINDOWS <arnav.salgarkar@gmail.com>
2026-04-18 23:55:28 +0530

    counting the occurrences of the word employment across the dataset

--- unfiltered/main.py
+++ unfiltered/main.py
@@ -22,3 +22,9 @@ education = df.headline_text.str.contains("education", case=True,
                                      na=None, regex=False)
 print(f"{df[education].shape[0]} headlines found!") # 9299 headlines
 print(f"{df[education].head(9299)}") # printing all the 9299 headlines found
+
+# counting the occurrences of the word "employment" across the dataset
+employment = df.headline_text.str.contains("employment", case=True,
+                                           na=None, regex=False)
+# print(f"{df[employment].shape[0]} headlines found!") # 1008 headlines found
+# print(f"{df[employment].head()}") # printing all the 1008 headlines found
